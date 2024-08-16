e4e74533930a5431e7e418d7ec01146afdb45a8d
Putra Iskandar <piskndar@gmail.com>
2024-08-16 09:31:38 +0800

    remove telegram func

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,25 @@
-/*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/telegram-esp32-motion-detection-arduino/
-  
-  Project created using Brian Lough's Universal Telegram Bot Library: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
-  Recontribute by piskndar for STASEC Project.
-*/
-
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include <DNSServer.h>
 #include <WebServer.h>
-#include <UniversalTelegramBot.h>
+//#include <UniversalTelegramBot.h>
 #include <ArduinoJson.h>
 #include <WiFiManager.h>
-/*
-#include <Keypad.h>
-
-const byte ROWS = 4;
-const byte COLS = 4;
-char keys[ROWS][COLS] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
-};
-byte rowPins[ROWS] = {14, 27, 26, 25};
-byte colPins[COLS] = {33, 32, 35, 32};
-
-Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS);
-
-unsigned long loopCount;
-unsigned long startTime;
-String msg;
-*/
-// Replace with your network credentials
-//const char* ssid = "SJ Wifi #27@unifi";
-//const char* password = "SJfreewifi2023";
 
 // Initialize Telegram BOT
-#define BOTtoken "7323854577:AAGJfewY7Eb0pknO1BTAQ2vdo-YETaCj5nM"  // your Bot Token (Get from Botfather)
+//#define BOTtoken "7323854577:AAGJfewY7Eb0pknO1BTAQ2vdo-YETaCj5nM"  // your Bot Token (Get from Botfather)
 
 // Use @myidbot to find out the chat ID of an individual or a group
 // Also note that you need to click "start" on a bot before it can
 // message you
-#define CHAT_ID "1823422500" //shahril
+//#define CHAT_ID "1823422500" //shahril
 //#define CHAT_ID1 "62086994" //sod
 //#define CHAT_ID2 "5189525146" //adi
 
 WiFiClientSecure client;
-UniversalTelegramBot bot(BOTtoken, client);
+//UniversalTelegramBot bot(BOTtoken, client);
 
-const int motionSensor = 13; // PIR Motion Sensor
+const int motionSensor = 27; // PIR Motion Sensor
 bool motionDetected = false;
 
 // Indicates when motion is detected
@@ -96,12 +65,6 @@ void setup() {
   // Set motionSensor pin as interrupt, assign interrupt function and set RISING mode
   attachInterrupt(digitalPinToInterrupt(motionSensor), detectsMovement, RISING);
 
-  // Attempt to connect to Wifi network:
-  //Serial.print("Connecting Wifi: ");
-  //Serial.println(ssid);
-
-  //WiFi.mode(WIFI_STA);
-  //WiFi.begin(ssid, password);
   client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
   
   //while (WiFi.status() != WL_CONNECTED) {
@@ -113,8 +76,6 @@ void setup() {
   //Serial.println("WiFi connected");
   //Serial.print("IP address: ");
   //Serial.println(WiFi.localIP());
-
-  bot.sendMessage(CHAT_ID, "Bot started up", "");
   //bot.sendMessage(CHAT_ID1, "Bot started up", "");
   //bot.sendMessage(CHAT_ID2, "Bot started up", "");
   digitalWrite(15, true);
@@ -122,7 +83,6 @@ void setup() {
 
 void loop() {
   if(motionDetected){
-    bot.sendMessage(CHAT_ID, "Motion detected!!", "");
     //bot.sendMessage(CHAT_ID1, "Motion detected!!", "");
     //bot.sendMessage(CHAT_ID2, "Motion detected!!", "");
     Serial.println("Motion Detected");
