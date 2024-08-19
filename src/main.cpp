c3668948475cccde56538ba000a7fa5da09661ca
piskndarial <piskndar@gmail.com>
2024-08-19 21:29:20 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,294 +1,81 @@
-/*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/telegram-esp32-motion-detection-arduino/
-  
-  Project created using Brian Lough's Universal Telegram Bot Library: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
-  Recontribute by piskndar for STASEC Project.
-*/
+#include <Wire.h>
 
-#include <WiFi.h>
-#include <WiFiClientSecure.h>
-#include <DNSServer.h>
-#include <WebServer.h>
-#include <UniversalTelegramBot.h>
-#include <ArduinoJson.h>
-#include <WiFiManager.h>
-#include "esp_camera.h"
-#include "soc/soc.h"
-#include "soc/rtc_cntl_reg.h"
+#define OV7670_I2C_ADDRESS 0x21 // OV7670 default I2C address
 
-// Initialize Telegram BOT
-#define BOTtoken "7323854577:AAGJfewY7Eb0pknO1BTAQ2vdo-YETaCj5nM"  // your Bot Token (Get from Botfather)
-
-// Use @myidbot to find out the chat ID of an individual or a group
-// Also note that you need to click "start" on a bot before it can
-// message you
-#define CHAT_ID "1823422500" //shahril
-//#define CHAT_ID1 "62086994" //sod
-//#define CHAT_ID2 "5189525146" //adi
-
-WiFiClientSecure client;
-UniversalTelegramBot bot(BOTtoken, client);
-
-const int motionSensor = 13; // PIR Motion Sensor
-bool motionDetected = false;
-bool sendPhoto = false;
-
-int botRequestDelay = 1000;
-unsigned long lastTimeBotRan;
-
-#define XCLK_GPIO_NUM     27
-#define SIOD_GPIO_NUM     25
-#define SIOC_GPIO_NUM     23
-
-#define Y9_GPIO_NUM       19
-#define Y8_GPIO_NUM       36
-#define Y7_GPIO_NUM       18
-#define Y6_GPIO_NUM       39
-#define Y5_GPIO_NUM       5
-#define Y4_GPIO_NUM       34
-#define Y3_GPIO_NUM       17
-#define Y2_GPIO_NUM       35
-#define VSYNC_GPIO_NUM    22
-#define HREF_GPIO_NUM     26
-#define PCLK_GPIO_NUM     21
-
-void configInitCamera(){
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-  config.grab_mode = CAMERA_GRAB_LATEST;
-  
-  if(psramFound()){
-    config.frame_size = FRAMESIZE_UXGA;
-    config.jpeg_quality = 10;  //0-63 lower number means higher quality
-    config.fb_count = 1;
-  } else {
-    config.frame_size = FRAMESIZE_SVGA;
-    config.jpeg_quality = 12;  //0-63 lower number means higher quality
-    config.fb_count = 1;
-  }
-  
-  // camera init
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    delay(1000);
-    ESP.restart();
-  }
-}
-
-void handleNewMessages(int numNewMessages) {
-  Serial.print("Handle New Messages: ");
-  Serial.println(numNewMessages);
-
-  for (int i = 0; i < numNewMessages; i++) {
-    String chat_id = String(bot.messages[i].chat_id);
-    if (chat_id != CHAT_ID){
-      bot.sendMessage(chat_id, "Unauthorized user", "");
-      continue;
-    }
-    
-    // Print the received message
-    String text = bot.messages[i].text;
-    Serial.println(text);
-    
-    String from_name = bot.messages[i].from_name;
-    if (text == "/start") {
-      String welcome = "Welcome , " + from_name + "\n";
-      welcome += "Use the following commands to interact with the ESP32-CAM \n";
-      welcome += "/photo : takes a new photo\n";
-      bot.sendMessage(CHAT_ID, welcome, "");
-    }
-    if (text == "/photo") {
-      sendPhoto = true;
-      Serial.println("New photo request");
-    }
-  }
-}
-
-String sendPhotoTelegram() {
-  const char* myDomain = "api.telegram.org";
-  String getAll = "";
-  String getBody = "";
-
-  //Dispose first picture because of bad quality
-  camera_fb_t * fb = NULL;
-  fb = esp_camera_fb_get();
-  esp_camera_fb_return(fb); // dispose the buffered image
-  
-  // Take a new photo
-  fb = NULL;  
-  fb = esp_camera_fb_get();  
-  if(!fb) {
-    Serial.println("Camera capture failed");
-    delay(1000);
-    ESP.restart();
-    return "Camera capture failed";
-  }  
-  
-  Serial.println("Connect to " + String(myDomain));
-
-
-  if (client.connect(myDomain, 443)) {
-    Serial.println("Connection successful");
-    
-    String head = "--RandomNerdTutorials\r\nContent-Disposition: form-data; name=\"chat_id\"; \r\n\r\n" + String(CHAT_ID) +"\r\n--RandomNerdTutorials\r\nContent-Disposition: form-data; name=\"photo\"; filename=\"esp32-cam.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n";
-    String tail = "\r\n--RandomNerdTutorials--\r\n";
-
-    size_t imageLen = fb->len;
-    size_t extraLen = head.length() + tail.length();
-    size_t totalLen = imageLen + extraLen;
-  
-    client.println("POST /bot"+ String(BOTtoken) +"/sendPhoto HTTP/1.1");
-    client.println("Host: " + String(myDomain));
-    client.println("Content-Length: " + String(totalLen));
-    client.println("Content-Type: multipart/form-data; boundary=RandomNerdTutorials");
-    client.println();
-    client.print(head);
-  
-    uint8_t *fbBuf = fb->buf;
-    size_t fbLen = fb->len;
-    for (size_t n=0;n<fbLen;n=n+1024) {
-      if (n+1024<fbLen) {
-        client.write(fbBuf, 1024);
-        fbBuf += 1024;
-      }
-      else if (fbLen%1024>0) {
-        size_t remainder = fbLen%1024;
-        client.write(fbBuf, remainder);
-      }
-    }  
-    
-    client.print(tail);
-    
-    esp_camera_fb_return(fb);
-    
-    int waitTime = 10000;   // timeout 10 seconds
-    long startTimer = millis();
-    boolean state = false;
-    
-    while ((startTimer + waitTime) > millis()){
-      Serial.print(".");
-      delay(100);      
-      while (client.available()) {
-        char c = client.read();
-        if (state==true) getBody += String(c);        
-        if (c == '\n') {
-          if (getAll.length()==0) state=true; 
-          getAll = "";
-        } 
-        else if (c != '\r')
-          getAll += String(c);
-        startTimer = millis();
-      }
-      if (getBody.length()>0) break;
-    }
-    client.stop();
-    Serial.println(getBody);
-  }
-  else {
-    getBody="Connected to api.telegram.org failed.";
-    Serial.println("Connected to api.telegram.org failed.");
-  }
-  return getBody;
-}
-
-
-// Indicates when motion is detected
-void IRAM_ATTR detectsMovement() {
-  //Serial.println("MOTION DETECTED!!!");
-  motionDetected = true;
-}
+// Define OV7670 pin connections
+#define VSYNC_PIN 25
+#define HREF_PIN 23
+#define PCLK_PIN 19
+#define XCLK_PIN 18
+#define D0_PIN 5
+#define D1_PIN 17
+#define D2_PIN 16
+#define D3_PIN 4
+#define D4_PIN 0
+#define D5_PIN 2
+#define D6_PIN 15
+#define D7_PIN 13
 
 void setup() {
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); 
+  Wire.begin();
   Serial.begin(115200);
-  configInitCamera();
-  /*
-  loopCount = 0;
-  startTime = millis();
-  msg = "";
-  */
-  pinMode(2 , OUTPUT);
-  pinMode(15, OUTPUT);
-  pinMode(4, OUTPUT);
-  /* Merah = 2
-     Hijau = 15
-     Biru = 4*/
+
+  // Initialize OV7670 camera
+  initializeCamera();
   
-
-  WiFiManager wm;
-  bool res;
-  res = wm.autoConnect("SJ Legacy ESP32 Free WiFi!","password");
-  if(!res) {
-        Serial.println("Failed to connect");
-
-  }
-  else {
-    Serial.println("Already connected!");
-    digitalWrite(2 ,false);
-    digitalWrite(15, false);
-    digitalWrite(4, true);
-    delay(1000);
-    digitalWrite(4, false);
-    digitalWrite(15, true);
+  // Initialize GPIO for camera data pins
+  pinMode(VSYNC_PIN, INPUT);
+  pinMode(HREF_PIN, INPUT);
+  pinMode(PCLK_PIN, INPUT);
+  for (int i = D0_PIN; i <= D7_PIN; i++) {
+    pinMode(i, INPUT);
   }
 
-  pinMode(motionSensor, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(motionSensor), detectsMovement, RISING);
-  client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
-  
-  bot.sendMessage(CHAT_ID, "Bot started up", "");
-  //bot.sendMessage(CHAT_ID1, "Bot started up", "");
-  //bot.sendMessage(CHAT_ID2, "Bot started up", "");
-  digitalWrite(15, true);
+  // Initialize XCLK for camera
+  ledcAttachPin(XCLK_PIN, 0);  // Use LEDC to generate clock signal
+  ledcSetup(0, 20000000, 1);   // 20MHz clock
+  ledcWrite(0, 1);
+
+  // Ready to capture image
+  Serial.println("Camera Initialized");
 }
 
 void loop() {
-  if (sendPhoto) {
-    Serial.println("Preparing photo");
-    sendPhotoTelegram(); 
-    sendPhoto = false; 
+  if (digitalRead(VSYNC_PIN) == HIGH) {
+    captureImage();
   }
-  if (millis() > lastTimeBotRan + botRequestDelay)  {
-    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
-    while (numNewMessages) {
-      Serial.println("got response");
-      handleNewMessages(numNewMessages);
-      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+}
+
+void initializeCamera() {
+  Wire.beginTransmission(OV7670_I2C_ADDRESS);
+  // Send initialization commands to the OV7670
+  Wire.write(0x12); // Reset the camera
+  Wire.write(0x80);
+  Wire.endTransmission();
+  
+  delay(100); // Wait for reset
+  
+  // More initialization commands can be added here
+}
+
+void captureImage() {
+  Serial.println("Capturing Image...");
+
+  while (digitalRead(VSYNC_PIN) == LOW); // Wait for VSYNC to go high
+
+  while (digitalRead(VSYNC_PIN) == HIGH) { // Image capture loop
+    if (digitalRead(HREF_PIN) == HIGH) {
+      // Read pixel data
+      uint8_t byte = 0;
+      for (int i = 0; i < 8; i++) {
+        byte |= digitalRead(D0_PIN + i) << i;
+      }
+      Serial.write(byte); // Send pixel byte to Serial for now (or save to buffer)
     }
-    lastTimeBotRan = millis();
+
+    while (digitalRead(PCLK_PIN) == LOW); // Wait for PCLK high
+    while (digitalRead(PCLK_PIN) == HIGH); // Wait for PCLK low
   }
-  if(motionDetected){
-    bot.sendMessage(CHAT_ID, "Motion detected!!", "");
-    //bot.sendMessage(CHAT_ID1, "Motion detected!!", "");
-    //bot.sendMessage(CHAT_ID2, "Motion detected!!", "");
-    Serial.println("Motion Detected");
-    motionDetected = false;
-    digitalWrite(15, false);
-    for (int i = 0; i < 5; i++) {
-      digitalWrite(2, true);
-      delay(100);
-      digitalWrite(2, false);
-      delay(100);
-    }
-    digitalWrite(15, true);
-  }
+
+  Serial.println("Image Captured");
 }
