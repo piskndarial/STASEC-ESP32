059afe40023d9022dce6ad66d8dc59c6221b669b
Putra Iskandar <piskndar@gmail.com>
2024-08-20 21:43:17 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,166 +1,68 @@
-/*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/telegram-esp32-motion-detection-arduino/
-  
-  Project created using Brian Lough's Universal Telegram Bot Library: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
-  Recontribute by piskndar for STASEC Project.
-*/
-
+#include "esp_camera.h"
 #include <WiFi.h>
-#include <WiFiClientSecure.h>
-#include <DNSServer.h>
-#include <WebServer.h>
-#include <UniversalTelegramBot.h>
-#include <ArduinoJson.h>
-#include <WiFiManager.h>
-/*
-#include <Keypad.h>
 
-const byte ROWS = 4;
-const byte COLS = 4;
-char keys[ROWS][COLS] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
-};
-byte rowPins[ROWS] = {14, 27, 26, 25};
-byte colPins[COLS] = {33, 32, 35, 32};
-
-Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS);
-
-unsigned long loopCount;
-unsigned long startTime;
-String msg;
-*/
 // Replace with your network credentials
-//const char* ssid = "SJ Wifi #27@unifi";
-//const char* password = "SJfreewifi2023";
-
-// Initialize Telegram BOT
-#define BOTtoken "7323854577:AAGJfewY7Eb0pknO1BTAQ2vdo-YETaCj5nM"  // your Bot Token (Get from Botfather)
-
-// Use @myidbot to find out the chat ID of an individual or a group
-// Also note that you need to click "start" on a bot before it can
-// message you
-#define CHAT_ID "1823422500" //shahril
-//#define CHAT_ID1 "62086994" //sod
-//#define CHAT_ID2 "5189525146" //adi
-
-WiFiClientSecure client;
-UniversalTelegramBot bot(BOTtoken, client);
-
-const int motionSensor = 13; // PIR Motion Sensor
-bool motionDetected = false;
-
-// Indicates when motion is detected
-void IRAM_ATTR detectsMovement() {
-  //Serial.println("MOTION DETECTED!!!");
-  motionDetected = true;
-}
+const char* ssid = "your_SSID";
+const char* password = "your_PASSWORD";
 
 void setup() {
-  Serial.begin(115200);
-  /*
-  loopCount = 0;
-  startTime = millis();
-  msg = "";
-  */
-  pinMode(2 , OUTPUT);
-  pinMode(15, OUTPUT);
-  pinMode(4, OUTPUT);
-  /* Merah = 2
-     Hijau = 15
-     Biru = 4*/
-  
+    Serial.begin(115200);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to WiFi...");
+    }
+    Serial.println("Connected to WiFi");
 
-  WiFiManager wm;
-  bool res;
-  res = wm.autoConnect("SJ Legacy ESP32 Free WiFi!","password");
-  if(!res) {
-        Serial.println("Failed to connect");
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = 32;
+    config.pin_d1 = 33;
+    config.pin_d2 = 34;
+    config.pin_d3 = 35;
+    config.pin_xclk = 23;
+    config.pin_pclk = 25;
+    config.pin_vsync = 26;
+    config.pin_href = 27;
+    config.pin_sccb_sda = 21;
+    config.pin_sccb_scl = 22;
+    config.pin_reset = -1;
+    config.xclk_freq_hz = 20000000;
+    config.pixel_format = PIXFORMAT_JPEG;
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12; // 0-63 lower number means higher quality
+    config.fb_count = 2; // if more than one, will use more RAM
 
-  }
-  else {
-    Serial.println("Already connected!");
-    digitalWrite(2 ,false);
-    digitalWrite(15, false);
-    digitalWrite(4, true);
-    delay(1000);
-    digitalWrite(4, false);
-    digitalWrite(15, true);
-  }
-
-  // PIR Motion Sensor mode INPUT_PULLUP
-  pinMode(motionSensor, INPUT_PULLUP);
-  // Set motionSensor pin as interrupt, assign interrupt function and set RISING mode
-  attachInterrupt(digitalPinToInterrupt(motionSensor), detectsMovement, RISING);
-
-  // Attempt to connect to Wifi network:
-  //Serial.print("Connecting Wifi: ");
-  //Serial.println(ssid);
-
-  //WiFi.mode(WIFI_STA);
-  //WiFi.begin(ssid, password);
-  client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
-  
-  //while (WiFi.status() != WL_CONNECTED) {
-    //Serial.print(".");
-    //delay(500);
-  //}
-
-  //Serial.println("");
-  //Serial.println("WiFi connected");
-  //Serial.print("IP address: ");
-  //Serial.println(WiFi.localIP());
-
-  bot.sendMessage(CHAT_ID, "Bot started up", "");
-  //bot.sendMessage(CHAT_ID1, "Bot started up", "");
-  //bot.sendMessage(CHAT_ID2, "Bot started up", "");
-  digitalWrite(15, true);
+    // Initialize the camera
+    esp_err_t err = esp_camera_init(&config);
+    if (err != ESP_OK) {
+        Serial.printf("Camera init failed with error 0x%x", err);
+        return;
+    }
 }
 
 void loop() {
-  if(motionDetected){
-    bot.sendMessage(CHAT_ID, "Motion detected!!", "");
-    //bot.sendMessage(CHAT_ID1, "Motion detected!!", "");
-    //bot.sendMessage(CHAT_ID2, "Motion detected!!", "");
-    Serial.println("Motion Detected");
-    motionDetected = false;
-    digitalWrite(15, false);
-    for (int i = 0; i < 5; i++) {
-      digitalWrite(2, true);
-      delay(100);
-      digitalWrite(2, false);
-      delay(100);
+    camera_fb_t *fb = esp_camera_fb_get();
+    if (!fb) {
+        Serial.println("Camera capture failed");
+        return;
     }
-    digitalWrite(15, true);
-  }
 
-  /*
-  if (kpd.getKeys())
-  {
-    for (int i=0; i<LIST_MAX; i++)
-    {
-      if (kpd.key[i].stateChanged)
-      {
-        switch (kpd.key[i].kstate) {
-          case PRESSED:
-          msg = "PRESSED.";
-        break;
-          case HOLD:
-          msg = "HOLD.";
-        break;
-          case RELEASED:
-          msg = "RELEASED.";
-        break;
-          case IDLE:
-          msg = "IDLE.";
-        }
-        Serial.print("KEY ");
-        Serial.print(kpd.key[i].kchar);
-        Serial.println(msg); 
-      }
+    // Send image to server
+    WiFiClient client;
+    if (client.connect("your.server.com", 80)) {
+        String header = "--boundary\r\n";
+        header += "Content-Disposition: form-data; name=\"file\"; filename=\"image.jpg\"\r\n";
+        header += "Content-Type: image/jpeg\r\n\r\n";
+        client.print(header);
+        client.write(fb->buf, fb->len);
+        client.print("\r\n--boundary--\r\n");
+        Serial.println("Image sent");
+    } else {
+        Serial.println("Failed to connect to server");
     }
-  }*/
+
+    esp_camera_fb_return(fb);
+    delay(5000); // Capture every 5 seconds
 }
