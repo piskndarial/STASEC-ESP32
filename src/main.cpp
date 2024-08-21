f8cec3e72b4a1c52f90d78830c66bf37d1d0f96f
Putra Iskandar <piskndar@gmail.com>
2024-08-21 16:48:13 +0800

    alr

--- src/main.cpp
+++ src/main.cpp
@@ -1,166 +1,157 @@
-/*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/telegram-esp32-motion-detection-arduino/
-  
-  Project created using Brian Lough's Universal Telegram Bot Library: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
-  Recontribute by piskndar for STASEC Project.
-*/
-
+#include "Arduino.h"
+#include "esp_camera.h"
 #include <WiFi.h>
-#include <WiFiClientSecure.h>
-#include <DNSServer.h>
-#include <WebServer.h>
-#include <UniversalTelegramBot.h>
-#include <ArduinoJson.h>
-#include <WiFiManager.h>
-/*
-#include <Keypad.h>
 
-const byte ROWS = 4;
-const byte COLS = 4;
-char keys[ROWS][COLS] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
-};
-byte rowPins[ROWS] = {14, 27, 26, 25};
-byte colPins[COLS] = {33, 32, 35, 32};
+//
+// WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
+//            Ensure ESP32 Wrover Module or other board with PSRAM is selected
+//            Partial images will be transmitted if image exceeds buffer size
+//
+//            You must select partition scheme from the board menu that has at least 3MB APP space.
+//            Face Recognition is DISABLED for ESP32 and ESP32-S2, because it takes up from 15
+//            seconds to process single frame. Face Detection is ENABLED if PSRAM is enabled as well
 
-Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+// ===================
+// Select camera model
+// ===================
+//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+#define CAMERA_MODEL_ESP_EYE  // Has PSRAM
+//#define CAMERA_MODEL_ESP32S3_EYE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
+//#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
+//#define CAMERA_MODEL_M5STACK_UNITCAM // No PSRAM
+//#define CAMERA_MODEL_M5STACK_CAMS3_UNIT  // Has PSRAM
+//#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
+//#define CAMERA_MODEL_XIAO_ESP32S3 // Has PSRAM
+// ** Espressif Internal Boards **
+//#define CAMERA_MODEL_ESP32_CAM_BOARD
+//#define CAMERA_MODEL_ESP32S2_CAM_BOARD
+//#define CAMERA_MODEL_ESP32S3_CAM_LCD
+//#define CAMERA_MODEL_DFRobot_FireBeetle2_ESP32S3 // Has PSRAM
+//#define CAMERA_MODEL_DFRobot_Romeo_ESP32S3 // Has PSRAM
+#include "camera_pins.h"
 
-unsigned long loopCount;
-unsigned long startTime;
-String msg;
-*/
-// Replace with your network credentials
-//const char* ssid = "SJ Wifi #27@unifi";
-//const char* password = "SJfreewifi2023";
+// ===========================
+// Enter your WiFi credentials
+// ===========================
+const char *ssid = "**********";
+const char *password = "**********";
 
-// Initialize Telegram BOT
-#define BOTtoken "7323854577:AAGJfewY7Eb0pknO1BTAQ2vdo-YETaCj5nM"  // your Bot Token (Get from Botfather)
-
-// Use @myidbot to find out the chat ID of an individual or a group
-// Also note that you need to click "start" on a bot before it can
-// message you
-#define CHAT_ID "1823422500" //shahril
-//#define CHAT_ID1 "62086994" //sod
-//#define CHAT_ID2 "5189525146" //adi
-
-WiFiClientSecure client;
-UniversalTelegramBot bot(BOTtoken, client);
-
-const int motionSensor = 13; // PIR Motion Sensor
-bool motionDetected = false;
-
-// Indicates when motion is detected
-void IRAM_ATTR detectsMovement() {
-  //Serial.println("MOTION DETECTED!!!");
-  motionDetected = true;
-}
+void startCameraServer();
+void setupLedFlash(int pin);
 
 void setup() {
   Serial.begin(115200);
-  /*
-  loopCount = 0;
-  startTime = millis();
-  msg = "";
-  */
-  pinMode(2 , OUTPUT);
-  pinMode(15, OUTPUT);
-  pinMode(4, OUTPUT);
-  /* Merah = 2
-     Hijau = 15
-     Biru = 4*/
-  
+  Serial.setDebugOutput(true);
+  Serial.println();
 
-  WiFiManager wm;
-  bool res;
-  res = wm.autoConnect("SJ Legacy ESP32 Free WiFi!","password");
-  if(!res) {
-        Serial.println("Failed to connect");
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.frame_size = FRAMESIZE_UXGA;
+  config.pixel_format = PIXFORMAT_JPEG;  // for streaming
+  //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
 
+  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  //                      for larger pre-allocated frame buffer.
+  if (config.pixel_format == PIXFORMAT_JPEG) {
+    if (psramFound()) {
+      config.jpeg_quality = 10;
+      config.fb_count = 2;
+      config.grab_mode = CAMERA_GRAB_LATEST;
+    } else {
+      // Limit the frame size when PSRAM is not available
+      config.frame_size = FRAMESIZE_SVGA;
+      config.fb_location = CAMERA_FB_IN_DRAM;
+    }
+  } else {
+    // Best option for face detection/recognition
+    config.frame_size = FRAMESIZE_240X240;
+#if CONFIG_IDF_TARGET_ESP32S3
+    config.fb_count = 2;
+#endif
   }
-  else {
-    Serial.println("Already connected!");
-    digitalWrite(2 ,false);
-    digitalWrite(15, false);
-    digitalWrite(4, true);
-    delay(1000);
-    digitalWrite(4, false);
-    digitalWrite(15, true);
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
   }
 
-  // PIR Motion Sensor mode INPUT_PULLUP
-  pinMode(motionSensor, INPUT_PULLUP);
-  // Set motionSensor pin as interrupt, assign interrupt function and set RISING mode
-  attachInterrupt(digitalPinToInterrupt(motionSensor), detectsMovement, RISING);
+  sensor_t *s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1);        // flip it back
+    s->set_brightness(s, 1);   // up the brightness just a bit
+    s->set_saturation(s, -2);  // lower the saturation
+  }
+  // drop down frame size for higher initial frame rate
+  if (config.pixel_format == PIXFORMAT_JPEG) {
+    s->set_framesize(s, FRAMESIZE_QVGA);
+  }
 
-  // Attempt to connect to Wifi network:
-  //Serial.print("Connecting Wifi: ");
-  //Serial.println(ssid);
+#if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
+  s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
+#endif
 
-  //WiFi.mode(WIFI_STA);
-  //WiFi.begin(ssid, password);
-  client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
-  
-  //while (WiFi.status() != WL_CONNECTED) {
-    //Serial.print(".");
-    //delay(500);
-  //}
+#if defined(CAMERA_MODEL_ESP32S3_EYE)
+  s->set_vflip(s, 1);
+#endif
 
-  //Serial.println("");
-  //Serial.println("WiFi connected");
-  //Serial.print("IP address: ");
-  //Serial.println(WiFi.localIP());
+// Setup LED FLash if LED pin is defined in camera_pins.h
+#if defined(LED_GPIO_NUM)
+  setupLedFlash(LED_GPIO_NUM);
+#endif
 
-  bot.sendMessage(CHAT_ID, "Bot started up", "");
-  //bot.sendMessage(CHAT_ID1, "Bot started up", "");
-  //bot.sendMessage(CHAT_ID2, "Bot started up", "");
-  digitalWrite(15, true);
+  WiFi.begin(ssid, password);
+  WiFi.setSleep(false);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
 }
 
 void loop() {
-  if(motionDetected){
-    bot.sendMessage(CHAT_ID, "Motion detected!!", "");
-    //bot.sendMessage(CHAT_ID1, "Motion detected!!", "");
-    //bot.sendMessage(CHAT_ID2, "Motion detected!!", "");
-    Serial.println("Motion Detected");
-    motionDetected = false;
-    digitalWrite(15, false);
-    for (int i = 0; i < 5; i++) {
-      digitalWrite(2, true);
-      delay(100);
-      digitalWrite(2, false);
-      delay(100);
-    }
-    digitalWrite(15, true);
-  }
-
-  /*
-  if (kpd.getKeys())
-  {
-    for (int i=0; i<LIST_MAX; i++)
-    {
-      if (kpd.key[i].stateChanged)
-      {
-        switch (kpd.key[i].kstate) {
-          case PRESSED:
-          msg = "PRESSED.";
-        break;
-          case HOLD:
-          msg = "HOLD.";
-        break;
-          case RELEASED:
-          msg = "RELEASED.";
-        break;
-          case IDLE:
-          msg = "IDLE.";
-        }
-        Serial.print("KEY ");
-        Serial.print(kpd.key[i].kchar);
-        Serial.println(msg); 
-      }
-    }
-  }*/
+  // Do nothing. Everything is done in another task by the web server
+  delay(10000);
 }
