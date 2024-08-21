5807a5f18e8f88c0d69c6a67d922f27ca5857523
piskndarial <piskndar@gmail.com>
2024-08-22 07:03:06 +0800

    OK

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
+#include <WiFi.h>
+#include <esp_wifi.h>
+#include <lwip/err.h>
+#include <lwip/sockets.h>
+#include "BMP.h"
+#include "OV7670.h"
+#include "wifi_internal.h"
 #include <esp_camera.h>
 #include <Arduino.h>
 #include <Wire.h>
-#include "esp_camera.h"
 #include <esp_log.h>
 #include <esp_system.h>
 #include <nvs_flash.h>
@@ -16,42 +22,119 @@
 #include "soc/rtc_cntl_reg.h"
 #include <UniversalTelegramBot.h>
 #include <ArduinoJson.h>
+const int SIOD = SDA;
+const int SIOC = SCL;
+
+const int VSYNC = 32;
+const int HREF = 39;
+
+const int XCLK = 18;
+const int PCLK = 36;
+
+const int D0 = 12;
+const int D1 = 14;
+const int D2 = 27;
+const int D3 = 26;
+const int D4 = 25;
+const int D5 = 35;
+const int D6 = 34;
+const int D7 = 33;
+
+#define SSID "Test"
+
+#define MAX_PACKET_SIZE 1460
+#define MAX_DATA_SIZE 1450
+
+struct PacketHeader {
+  uint8_t frame;
+  uint32_t offset;
+  uint32_t len;
+};
+
+OV7670 *camera;
+
+byte bmpHeader[BMP::headerSize];
+int udp_server = -1;
+struct sockaddr_in destination;
+bool video_running = false;
 
 void setup() {
-  Wire.begin(18, 17);  // Replace with your SDA and SCL pins
   Serial.begin(115200);
-  Serial.println("\nI2C Scanner");
+
+  WiFi.disconnect(true);
+
+  // Force data rate
+  wifi_internal_rate_t rate;
+  rate.fix_rate = RATE_MCS4_SP;
+  esp_wifi_internal_set_rate(100, 1, 4, &rate);
+
+  WiFi.softAP(SSID);
+  WiFi.onEvent(onClientChange, SYSTEM_EVENT_AP_STACONNECTED);
+  WiFi.onEvent(onClientChange, SYSTEM_EVENT_AP_STADISCONNECTED);
+
+  // Change beacon_interval because 100ms is crazy!
+  wifi_config_t conf;
+  esp_wifi_get_config(WIFI_IF_AP, &conf);
+  conf.ap.beacon_interval = 3000;
+  esp_wifi_set_config(WIFI_IF_AP, &conf);
+  
+  camera = new OV7670(OV7670::Mode::QQVGA_RGB565, SIOD, SIOC, VSYNC, HREF, XCLK, PCLK, D0, D1, D2, D3, D4, D5, D6, D7);
+  BMP::construct16BitHeader(bmpHeader, camera->xres, camera->yres);
+
+  // Init socket
+  udp_server = socket(AF_INET, SOCK_DGRAM, 0);
+  destination.sin_addr.s_addr = (uint32_t)IPAddress(255,255,255,255);
+  destination.sin_family = AF_INET;
+  destination.sin_port = htons(3333);
+
+  Serial.println("Up, Waiting for clients");
+}
+
+void onClientChange(system_event_id_t event) {
+  // Only start sending video after a client connects to avoid flooding
+  // the channel when the client is attempting to connect.
+  video_running = WiFi.softAPgetStationNum() != 0;
+  if (video_running) {
+    Serial.println("Video started!");
+  } else {
+    Serial.println("STOPPED");
+  }
 }
 
 void loop() {
-  byte error, address;
-  int nDevices;
-
-  Serial.println("Scanning...");
-
-  nDevices = 0;
-  for (address = 1; address < 127; address++) {
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
-
-    if (error == 0) {
-      Serial.print("I2C device found at address 0x");
-      if (address < 16)
-        Serial.print("0");
-      Serial.print(address, HEX);
-      Serial.println(" !");
-      nDevices++;
-    } else if (error == 4) {
-      Serial.print("Unknown error at address 0x");
-      if (address < 16)
-        Serial.print("0");
-      Serial.println(address, HEX);
-    }
+  if (!video_running) {
+    return;
   }
-  if (nDevices == 0)
-    Serial.println("No I2C devices found\n");
-  else
-    Serial.println("done\n");
 
-  delay(5000);
+  camera->oneFrame();
+
+  static byte packet[MAX_PACKET_SIZE];
+  static uint8_t framenum = 0;
+  PacketHeader* header = (PacketHeader*)packet;
+  header->frame = framenum++;
+  header->len = camera->frameBytes;
+
+  for (int i = 0; i < camera->frameBytes; i += MAX_DATA_SIZE) {
+    bool end = i + MAX_DATA_SIZE > camera->frameBytes;
+    int len = end ? camera->frameBytes - i : MAX_DATA_SIZE;
+      header->offset = i;
+      memcpy(packet + sizeof(PacketHeader), camera->frame + i, len);
+    int sent = sendto(
+      udp_server,
+      packet, sizeof(PacketHeader) + len,
+      0,
+      (struct sockaddr*) &destination, sizeof(destination)
+    );
+  }
+
+  // FPS counter
+  static unsigned int frames = 0;
+  static unsigned long last = millis();
+  ++frames;
+  unsigned long now = millis();
+  if (now - last > 1000) {
+    Serial.println(frames);
+    last = now;
+    frames = 0;
+  }
 }
