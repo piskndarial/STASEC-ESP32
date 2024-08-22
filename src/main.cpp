24930f6d1d7048c303ce77b1ef24545b7488213e
piskndarial <piskndar@gmail.com>
2024-08-22 11:00:26 +0800

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
-#include <WiFi.h>
-#include <esp_wifi.h>
-#include <lwip/err.h>
-#include <lwip/sockets.h>
-#include "BMP.h"
-#include "OV7670.h"
-#include "wifi_internal.h"
 #include <esp_camera.h>
 #include <Arduino.h>
 #include <Wire.h>
+#include "esp_camera.h"
 #include <esp_log.h>
 #include <esp_system.h>
 #include <nvs_flash.h>
@@ -22,121 +16,84 @@
 #include "soc/rtc_cntl_reg.h"
 #include <UniversalTelegramBot.h>
 #include <ArduinoJson.h>
-const int SIOD = SDA;
-const int SIOC = SCL;
 
-const int VSYNC = 32;
-const int HREF = 39;
+#define OV7670_I2C_ADDRESS 0x21 // OV7670 default I2C address
 
-const int XCLK = 18;
-const int PCLK = 36;
+// Define OV7670 pin connections
+#define VSYNC_PIN 25
+#define HREF_PIN 23
+#define PCLK_PIN 19
+#define XCLK_PIN 18
+#define D0_PIN 5
+#define D1_PIN 17
+#define D2_PIN 16
+#define D3_PIN 4
+#define D4_PIN 0
+#define D5_PIN 2
+#define D6_PIN 15
+#define D7_PIN 13
+void captureImage() {
+  Serial.println("Capturing Image...");
 
-const int D0 = 12;
-const int D1 = 14;
-const int D2 = 27;
-const int D3 = 26;
-const int D4 = 25;
-const int D5 = 35;
-const int D6 = 34;
-const int D7 = 33;
+  while (digitalRead(VSYNC_PIN) == LOW); // Wait for VSYNC to go high
 
-#define SSID "Test"
+  while (digitalRead(VSYNC_PIN) == HIGH) { // Image capture loop
+    if (digitalRead(HREF_PIN) == HIGH) {
+      // Read pixel data
+      uint8_t byte = 0;
+      for (int i = 0; i < 8; i++) {
+        byte |= digitalRead(D0_PIN + i) << i;
+      }
+      Serial.write(byte); // Send pixel byte to Serial for now (or save to buffer)
+    }
 
-#define MAX_PACKET_SIZE 1460
-#define MAX_DATA_SIZE 1450
-
-struct PacketHeader {
-  uint8_t frame;
-  uint32_t offset;
-  uint32_t len;
-};
-
-OV7670 *camera;
-
-byte bmpHeader[BMP::headerSize];
-int udp_server = -1;
-struct sockaddr_in destination;
-bool video_running = false;
-
-void onClientChange(system_event_id_t event) {
-  // Only start sending video after a client connects to avoid flooding
-  // the channel when the client is attempting to connect.
-  video_running = WiFi.softAPgetStationNum() != 0;
-  if (video_running) {
-    Serial.println("Video started!");
-  } else {
-    Serial.println("STOPPED");
+    while (digitalRead(PCLK_PIN) == LOW); // Wait for PCLK high
+    while (digitalRead(PCLK_PIN) == HIGH); // Wait for PCLK low
   }
+
+  Serial.println("Image Captured");
 }
 
+void initializeCamera() {
+  Wire.beginTransmission(OV7670_I2C_ADDRESS);
+  // Send initialization commands to the OV7670
+  Wire.write(0x12); // Reset the camera
+  Wire.write(0x80);
+  Wire.endTransmission();
+  
+  delay(100); // Wait for reset
+  
+  // More initialization commands can be added here
+}
 void setup() {
+  Wire.begin();
   Serial.begin(115200);
 
-  WiFi.disconnect(true);
-
-  // Force data rate
-  wifi_internal_rate_t rate;
-  rate.fix_rate = RATE_MCS4_SP;
-  esp_wifi_internal_set_rate(100, 1, 4, &rate);
-
-WiFi.softAP(SSID);
-  WiFi.softAP(SSID);
-  WiFi.onEvent(onClientChange, SYSTEM_EVENT_AP_STACONNECTED);
-  WiFi.onEvent(onClientChange, SYSTEM_EVENT_AP_STADISCONNECTED);
-
-  // Change beacon_interval because 100ms is crazy!
-  wifi_config_t conf;
-  esp_wifi_get_config(WIFI_IF_AP, &conf);
-  conf.ap.beacon_interval = 3000;
-  esp_wifi_set_config(WIFI_IF_AP, &conf);
+  // Initialize OV7670 camera
+  initializeCamera();
   
-  camera = new OV7670(OV7670::Mode::QQVGA_RGB565, SIOD, SIOC, VSYNC, HREF, XCLK, PCLK, D0, D1, D2, D3, D4, D5, D6, D7);
-  BMP::construct16BitHeader(bmpHeader, camera->xres, camera->yres);
+  // Initialize GPIO for camera data pins
+  pinMode(VSYNC_PIN, INPUT);
+  pinMode(HREF_PIN, INPUT);
+  pinMode(PCLK_PIN, INPUT);
+  for (int i = D0_PIN; i <= D7_PIN; i++) {
+    pinMode(i, INPUT);
+  }
 
-  // Init socket
-  udp_server = socket(AF_INET, SOCK_DGRAM, 0);
-  destination.sin_addr.s_addr = (uint32_t)IPAddress(255,255,255,255);
-  destination.sin_family = AF_INET;
-  destination.sin_port = htons(3333);
+  // Initialize XCLK for camera
+  ledcAttachPin(XCLK_PIN, 0);  // Use LEDC to generate clock signal
+  ledcSetup(0, 20000000, 1);   // 20MHz clock
+  ledcWrite(0, 1);
 
-  Serial.println("Up, Waiting for clients");
+  // Ready to capture image
+  Serial.println("Camera Initialized");
 }
-
 
 void loop() {
-  if (!video_running) {
-    return;
-  }
-
-  camera->oneFrame();
-
-  static byte packet[MAX_PACKET_SIZE];
-  static uint8_t framenum = 0;
-  PacketHeader* header = (PacketHeader*)packet;
-  header->frame = framenum++;
-  header->len = camera->frameBytes;
-
-  for (int i = 0; i < camera->frameBytes; i += MAX_DATA_SIZE) {
-    bool end = i + MAX_DATA_SIZE > camera->frameBytes;
-    int len = end ? camera->frameBytes - i : MAX_DATA_SIZE;
-      header->offset = i;
-      memcpy(packet + sizeof(PacketHeader), camera->frame + i, len);
-    int sent = sendto(
-      udp_server,
-      packet, sizeof(PacketHeader) + len,
-      0,
-      (struct sockaddr*) &destination, sizeof(destination)
-    );
-  }
-
-  // FPS counter
-  static unsigned int frames = 0;
-  static unsigned long last = millis();
-  ++frames;
-  unsigned long now = millis();
-  if (now - last > 1000) {
-    Serial.println(frames);
-    last = now;
-    frames = 0;
+  if (digitalRead(VSYNC_PIN) == HIGH) {
+    captureImage();
   }
 }
+
+
+
