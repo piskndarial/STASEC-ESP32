b5d910bac952b3d78753faa30c8e5a1d3ba81341
piskndarial <piskndar@gmail.com>
2024-08-23 21:26:18 +0800

    bang buat apa tu bang

--- src/main.cpp
+++ src/main.cpp
@@ -43,8 +43,8 @@ String msg;
 // Use @myidbot to find out the chat ID of an individual or a group
 // Also note that you need to click "start" on a bot before it can
 // message you
-#define CHAT_ID "1823422500"
-#define CHAT_IDG1 "-1002190410847" //shahril
+//#define CHAT_ID "1823422500"
+#define CHAT_IDG1 "-1002190410847" //group
 //#define CHAT_ID1 "62086994" //cg saiful
 //#define CHAT_ID2 "5189525146" //adi
 
@@ -115,7 +115,7 @@ void setup() {
   //Serial.print("IP address: ");
   //Serial.println(WiFi.localIP());
 
-  bot.sendMessage(CHAT_ID, "Bot started up", "");
+  //bot.sendMessage(CHAT_ID, "Bot started up", "");
   bot.sendMessage(CHAT_IDG1, "Bot started up", "");
   //bot.sendMessage(CHAT_ID1, "Bot started up", "");
   //bot.sendMessage(CHAT_ID2, "Bot started up", "");
@@ -124,7 +124,7 @@ void setup() {
 
 void loop() {
   if(motionDetected){
-    bot.sendMessage(CHAT_ID, "Motion detected!!", "");
+    //bot.sendMessage(CHAT_ID, "Motion detected!!", "");
     bot.sendMessage(CHAT_IDG1, "Motion detected!!", "");
     //bot.sendMessage(CHAT_ID1, "Motion detected!!", "");
     //bot.sendMessage(CHAT_ID2, "Motion detected!!", "");
