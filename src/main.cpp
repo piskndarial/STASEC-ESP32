1733a7a0c2feddf1d2bf233d7c6620fac5f89d67
Putra Iskandar <piskndar@gmail.com>
2024-08-23 09:09:10 +0800

    yes

--- src/main.cpp
+++ src/main.cpp
@@ -31,10 +31,10 @@ const int TFT_CS = 5;
 //DIN <- MOSI 23
 //CLK <- SCK 18
 
-#define ssid1        "YOUR_WIFI_SSID"
-#define password1    "YOUR_PASSWORD"
-//#define ssid2        ""
-//#define password2    ""
+#define ssid1        "amir's lappy"
+#define password1    "12345678"
+#define ssid2        "SJ Wifi #51"
+#define password2    "SJfreewifi2023"
 
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, 0/*no reset*/);
 OV7670 *camera;
@@ -103,7 +103,7 @@ void setup()
   Serial.begin(115200);
 
   wifiMulti.addAP(ssid1, password1);
-  //wifiMulti.addAP(ssid2, password2);
+  wifiMulti.addAP(ssid2, password2);
   Serial.println("Connecting Wifi...");
   if(wifiMulti.run() == WL_CONNECTED) {
       Serial.println("");
